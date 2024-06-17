527c89866fb5ed9217ab662faa0736e3853f3a9d
Furkan <salmanfurkan28@gmail.com>
2024-06-17 07:28:26 +0300

    home and poem pages added, poem model added.

--- base/models.py
+++ base/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Poem(models.Model):
+    # By default, null is False, so we don't need to specify it
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)    
+    content = models.TextField()
+    author = models.CharField(max_length=50)
+    # Auto now add will automatically add the date when the object is created
+    created = models.DateTimeField(auto_now_add=True)
+    # Auto now will automatically update the date when the object is updated
+    updated = models.DateTimeField(auto_now=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.title + ' by ' + self.author
--- base/views.py
+++ base/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Poem
 
 # Create your views here.
 
+def home(request):
+    # Objects is a manager that allows us to interact with the database
+    # all() is a method that returns all the objects in the database
+    # get() is a method that returns a single object that matches the query
+    # filter() is a method that returns a list of objects that match the query
+    # exclude() is a method that returns a list of objects that don't match the query
+    poems = Poem.objects.all()
+    context = {
+        'poems': poems
+    }
+    return render(request, 'base/home.html', context)
+
+def poem(request, pk):
+    poem = Poem.objects.get(id=pk)
+    context = {
+        'poem': poem
+    }
+    return render(request, 'base/poem.html', context)
